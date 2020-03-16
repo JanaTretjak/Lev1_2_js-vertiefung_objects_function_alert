10e8e63f6e4a09a5d2f86e9dc0ef63d35dce0ff3
JanaTretjak <tretiachechka@gmail.com>
2020-03-16 13:46:02 +0100

    ver 0

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+let person = {
+    name: "Jana Tretjak",
+    alter: 29,
+    sagNameAlter() {
+        alert(`${this.name} ist ${this.alter}`)
+    },
+
+}
+person.sagNameAlter()
+console.log(person.name)
+console.log(person.alter)
